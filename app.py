2a1fb51574d265e90db035884acfd6ae9f088087
QuangQuyen0703 <155425427+QuangQuyen0703@users.noreply.github.com>
2024-01-23 16:37:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -129,17 +129,15 @@ if 'data' in locals() and not data.empty:
     create_column_chart(fig_active_customer_chart, processed_data['Year'], processed_data['active_customer'], 'Active Customers (Unit: Thousand)')
     st.plotly_chart(fig_active_customer_chart)
 
-    # Create a table for Customer Base
     st.subheader('Customer Base (Unit: Thousand Customer)')
-    customer_base_data = {
-        'Year': processed_data['Year'],
-        'New Customers': processed_data['New Customer'].round(0),
-        'Total Customers': processed_data['Total Customer'].round(0),
-        '% Active Rate': processed_data['Active Rate'].round(2),
-        'Active Customers': processed_data['active_customer'].round(0),
-        '% Funding Rate': processed_data['Funding Rate'].round(2),
-        'Funded Customer': processed_data['Funded Customer'].round(0)
-    }
+customer_base_data = {
+    'New Customers': processed_data['New Customer'].round(0),
+    'Total Customers': processed_data['Total Customer'].round(0),
+    '% Active Rate': processed_data['Active Rate'].round(2),
+    'Active Customers': processed_data['active_customer'].round(0),
+    '% Funding Rate': processed_data['Funding Rate'].round(2),
+    'Funded Customer': processed_data['Funded Customer'].round(0)
+}
 
-    customer_base_table = pd.DataFrame(customer_base_data)
-    st.table(customer_base_table)
+customer_base_table = pd.DataFrame(customer_base_data, index=processed_data['Year'])
+st.table(customer_base_table)
