c64c057d81aa79ba8f2f881df4ec51e5c49c2885
QuangQuyen0703 <155425427+QuangQuyen0703@users.noreply.github.com>
2024-01-23 23:06:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def calculate_metrics(data, funded_cac_increase, new_customer_increases2024, new
 
     # Calculate Revenue, GP/Active, total gross profit, LTV, LTV/CAC, Payback
     
-    data['Funded Customer'] = data['Total Customer'] * (data['Active Rate']/100) * (data['Funding Rate']/100)
+    data['Funded Customer'] = data['active_customer'] * (data['Funding Rate']/100)
     
     data['revenue'] = data['ARPU'] * data['active_customer'] / 1000
     data['gp_per_active'] = (data['ARPU'] - data['Direct Cost'])
