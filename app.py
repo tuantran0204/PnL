ff493b7fa4b5e4541fe5ea159568d85550b50ee0
QuangQuyen0703 <155425427+QuangQuyen0703@users.noreply.github.com>
2024-01-23 16:56:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -60,8 +60,6 @@ def calculate_metrics(data, funded_cac_increase, new_customer_increases2024, new
 
     return data
 
-
-
 # Title of the app
 st.title('PnL Simulator')
 
@@ -78,11 +76,11 @@ data = pd.read_csv("./data.csv")
 if 'data' in locals() and not data.empty:
     # Input for Funded CAC increase from 5 to 30
     funded_cac_increase = st.sidebar.number_input('Funded CAC 2024-2028 (Unit: $)', min_value=3, max_value=50, step=1, value=10)
-    new_customer_increases2024 = st.sidebar.number_input('New Customer 2024 (Unit: Thousand)', min_value=100, max_value=3000, step=50, value= 400)
-    new_customer_increases2025 = st.sidebar.number_input('New Customer 2025 (Unit: Thousand)', min_value=100, max_value=3000, step=50, value= 400)
-    new_customer_increases2026 = st.sidebar.number_input('New Customer 2026 (Unit: Thousand)', min_value=100, max_value=3000, step=50, value= 500)
-    new_customer_increases2027 = st.sidebar.number_input('New Customer 2027 (Unit: Thousand)', min_value=100, max_value=3000, step=50, value= 600)
-    new_customer_increases2028 = st.sidebar.number_input('New Customer 2028 (Unit: Thousand)', min_value=100, max_value=3000, step=50, value= 700)
+    new_customer_increases2024 = st.sidebar.number_input('New Customer 2024 (Unit: Thousand)', min_value=100, max_value=3000, step=50, value=400)
+    new_customer_increases2025 = st.sidebar.number_input('New Customer 2025 (Unit: Thousand)', min_value=100, max_value=3000, step=50, value=400)
+    new_customer_increases2026 = st.sidebar.number_input('New Customer 2026 (Unit: Thousand)', min_value=100, max_value=3000, step=50, value=500)
+    new_customer_increases2027 = st.sidebar.number_input('New Customer 2027 (Unit: Thousand)', min_value=100, max_value=3000, step=50, value=600)
+    new_customer_increases2028 = st.sidebar.number_input('New Customer 2028 (Unit: Thousand)', min_value=100, max_value=3000, step=50, value=700)
 
     new_customer_increases = [new_customer_increases2024, new_customer_increases2025, new_customer_increases2026, new_customer_increases2027, new_customer_increases2028]
 
@@ -96,29 +94,29 @@ if 'data' in locals() and not data.empty:
     # Visualization
     st.subheader(' Metrics Visualization:')
 
-    # Corrected indentation for the next block
-    st.subheader('Customer Base (Unit: Thousand Customers)')
-    customer_base_data = {
-    'Year': processed_data['Year'],
-    'New Customers': processed_data['New Customer'].round(2),
-    'Total Customers': processed_data['Total Customer'].round(2),
-    '%Active Rate': processed_data['Active Rate'].round(2),
-    'Active Customers': processed_data['active_customer'].round(2),
-    '%Funding Rate': processed_data['Funding Rate'].round(2),
-    'Funded Customer': processed_data['Funded Customer'].round(2)
-    }
-
     def create_column_chart(fig, x, y, title):
-    # Add trace to the column chart with a different color
-    fig.add_trace(go.Bar(x=x, y=y,
+        # Add trace to the column chart with a different color
+        fig.add_trace(go.Bar(x=x, y=y,
                              name=title,
                              marker_color='#563D82',
                              text=y.round(2),
                              textposition='outside'))
 
-    fig.update_layout(title=title)
-    fig.update_xaxes(showgrid=False)  # Remove x-axis gridlines
-    fig.update_yaxes(showgrid=False)  # Remove y-axis gridlines
+        fig.update_layout(title=title)
+        fig.update_xaxes(showgrid=False)  # Remove x-axis gridlines
+        fig.update_yaxes(showgrid=False)  # Remove y-axis gridlines
+
+    # Corrected indentation
+    st.subheader('Customer Base (Unit: Thousand Customers)')
+    customer_base_data = {
+        'Year': processed_data['Year'],
+        'New Customers': processed_data['New Customer'].round(2),
+        'Total Customers': processed_data['Total Customer'].round(2),
+        '%Active Rate': processed_data['Active Rate'].round(2),
+        'Active Customers': processed_data['active_customer'].round(2),
+        '%Funding Rate': processed_data['Funding Rate'].round(2),
+        'Funded Customer': processed_data['Funded Customer'].round(2)
+    }
 
     customer_base_table = pd.DataFrame(customer_base_data)
     st.table(customer_base_table)
@@ -137,8 +135,3 @@ if 'data' in locals() and not data.empty:
     fig_active_customer_chart = go.Figure()
     create_column_chart(fig_active_customer_chart, processed_data['Year'], processed_data['active_customer'], 'Active Customers (Unit: Thousand)')
     st.plotly_chart(fig_active_customer_chart)
-
-
-
-
-
