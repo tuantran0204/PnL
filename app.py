ef08d38db1726d3699cad065781f823625d1ad29
QuangQuyen0703 <155425427+QuangQuyen0703@users.noreply.github.com>
2024-01-23 20:53:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ fig_customer_acquisition_column.add_trace(go.Bar(x=processed_data['Year'], y=pro
                                                  textposition='outside'))
 
 fig_customer_acquisition_column.add_trace(go.Scatter(x=processed_data['Year'], y=processed_data['Funding Rate'],
-                                                      name='Funding Rate',
+                                                      name='% Funding Rate',
                                                       mode='lines+text',
                                                       line=dict(color='#EB3300'),
                                                       text=processed_data['Funding Rate'].round(2),
@@ -182,10 +182,10 @@ fig_customer_acquisition_column.add_trace(go.Scatter(x=processed_data['Year'], y
 fig_customer_acquisition_column.update_layout(barmode='group', title='Customer Acquisition (Unit: Thousand)')
 fig_customer_acquisition_column.update_layout(legend=dict(traceorder='normal', y=-0.15, x=0.5, orientation="h"))
 fig_customer_acquisition_column.update_layout(
-    yaxis2=dict(, overlaying='y', side='right', showgrid=False, title_font=dict(color='#EB3300'))
+    yaxis2=dict( overlaying='y', side='right', showgrid=False)
 )
 fig_customer_acquisition_column.update_xaxes(showgrid=False)  # Remove x-axis gridlines
-fig_customer_acquisition_column.update_yaxes(showgrid=False, title_font=dict(color='#563D82'))
+fig_customer_acquisition_column.update_yaxes(showgrid=False)
 st.plotly_chart(fig_customer_acquisition_column)
 
     
