0b9cb6251468c359ca71fa5da2e364390b0172b2
QuangQuyen0703 <155425427+QuangQuyen0703@users.noreply.github.com>
2024-01-23 19:07:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -162,11 +162,11 @@ if show_financial_metrics:
     fig_margin_chart.add_trace(go.Scatter(x=processed_data['Year'], y=processed_data['Gross Margin'],
                                         mode='lines+text', name='Gross Margin', line=dict(color='#EB3300'),
                                         text=processed_data['Gross Margin'].round(2),
-                                        textposition='top left', textfont=dict(color='#A9A9A9')))
+                                        textposition='top left', textfont=dict(color='#2774AE')))
     fig_margin_chart.add_trace(go.Scatter(x=processed_data['Year'], y=processed_data['EBIT Margin'],
                                         mode='lines+text', name='EBIT Margin', line=dict(color='#EB3300'),
                                         text=processed_data['EBIT Margin'].round(2),
-                                        textposition='below left', textfont=dict(color='#7F7F7F')))
+                                        textposition='bottom left', textfont=dict(color='#7F7F7F')))
 
     fig_margin_chart.update_layout(barmode='group', title='Gross Margin vs EBIT Margin (%)')
     fig_margin_chart.update_xaxes(showgrid=False)  # Remove x-axis gridlines
